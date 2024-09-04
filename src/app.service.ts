24fa939a5ea8a93fdd2e9174df2d638fb7faab82
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2024-09-04 15:06:46 +0700

    remove auth token endpoint info

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,15 +10,15 @@ export class AppService {
     private readonly jwt: JwtService,
   ) {}
   async getInfoApi(): Promise<IInfoApiType> {
-    const authCode = await this.prisma.auth_code.findFirst({});
-    const authorizationToken: string = this.jwt.sign(authCode, {
-      expiresIn: '30d',
-    });
+    // const authCode = await this.prisma.auth_code.findFirst({});
+    // const authorizationToken: string = this.jwt.sign(authCode, {
+    //   expiresIn: '30d',
+    // });
 
     return {
       name: 'Pinterest API',
       version: '1.0.0',
-      authorizationToken,
+      // authorizationToken,
       author: 'Luca Dev',
       swagger: '/swagger',
       description: 'New Pinterest API',
